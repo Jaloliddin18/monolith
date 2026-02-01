6c33e1e8ba5110e9e720c5966d8fc6a2f07f48fc
Jaloliddin18 <jaloliddin.khonimkulov05@gmail.com>
2026-02-02 08:23:39 +0900

    fix: package installation & modify format standards

--- apps/monolith-api/src/app.service.ts
+++ apps/monolith-api/src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'This is MONOLITH API Server!';
-  }
+	getHello(): string {
+		return 'This is MONOLITH API Server!';
+	}
 }
