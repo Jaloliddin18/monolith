c4c58896bf32e304097ead99f50f5774737d89c7
Jaloliddin18 <jaloliddin.khonimkulov05@gmail.com>
2026-02-02 08:13:59 +0900

    feat: configure env variables

--- apps/monolith-batch/src/monolith-batch.module.ts
+++ apps/monolith-batch/src/monolith-batch.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MonolithBatchController } from './monolith-batch.controller';
 import { MonolithBatchService } from './monolith-batch.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [MonolithBatchController],
   providers: [MonolithBatchService],
 })
--- apps/monolith-batch/src/monolith-batch.service.ts
+++ apps/monolith-batch/src/monolith-batch.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class MonolithBatchService {
   getHello(): string {
-    return 'This is MONOLITH Batch Server!';
+    return 'This is MONOLITH BATCH Server!';
   }
 }
