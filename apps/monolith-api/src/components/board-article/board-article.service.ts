29d29a8dc1336e5e26d349407b6de2d3309212f9
Jaloliddin18 <jaloliddin.khonimkulov05@gmail.com>
2026-03-02 19:45:17 +0900

    feat: develop likeTargetBoardArticle GraphQL API

--- apps/monolith-api/src/components/board-article/board-article.service.ts
+++ apps/monolith-api/src/components/board-article/board-article.service.ts
@@ -22,6 +22,9 @@ import {
 } from '../../libs/dto/board-article/board-article.input';
 import { BoardArticleUpdate } from '../../libs/dto/board-article/board-article.update';
 import { ViewGroup } from '../../libs/enums/view.enum';
+import { LikeService } from '../like/like.service';
+import { LikeInput } from '../../libs/dto/like/like.input';
+import { LikeGroup } from '../../libs/enums/like.enum';
 
 @Injectable()
 export class BoardArticleService {
@@ -30,6 +33,7 @@ export class BoardArticleService {
 		private readonly boardArticleModel: Model<BoardArticle>,
 		private readonly viewService: ViewService,
 		private readonly memberService: MemberService,
+		private readonly likeService: LikeService,
 	) {}
 
 	public async createBoardArticle(
@@ -155,6 +159,34 @@ export class BoardArticleService {
 			throw new InternalServerErrorException(Message.NO_DATA_FOUND);
 
 		return result[0];
+	}
+
+	public async likeTargetBoardArticle(
+		memberId: ObjectId,
+		likeRefId: ObjectId,
+	): Promise<BoardArticle> {
+		const target: BoardArticle = await this.boardArticleModel
+			.findOne({
+				_id: likeRefId,
+				articleStatus: BoardArticleStatus.ACTIVE,
+			})
+			.exec();
+		if (!target) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
+
+		const input: LikeInput = {
+			memberId: memberId,
+			likeRefId: likeRefId,
+			likeGroup: LikeGroup.ARTICLE,
+		};
+		const modifier: number = await this.likeService.toggleLike(input);
+		const result = await this.boardArticleStatsEditor({
+			_id: likeRefId,
+			targetKey: 'articleLikes',
+			modifier: modifier,
+		});
+		if (!result)
+			throw new InternalServerErrorException(Message.SOMETHING_WENT_WRONG);
+		return result;
 	}
 
 	public async getAllBoardArticlesByAdmin(
